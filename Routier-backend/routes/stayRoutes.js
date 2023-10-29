b6d60bad6f09717f71d01af99678d1ab303892b9
Harsh Mungara <harsh.mungara2004@gmail.com>
2023-10-29 21:53:14 +0530

    resolving commit

--- Routier-backend/routes/stayRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const StayController = require('../controllers/stayController')
-
-router.get('./show', StayController.show)
-
-module.exports = router
