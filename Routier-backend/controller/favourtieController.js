fbabbc854e186cb8a8de1d783ec403f7502e8357
Harsh Mungara <harsh.mungara2004@gmail.com>
2023-11-11 12:41:31 +0530

    function changes in favourite feature

--- Routier-backend/controller/favourtieController.js
+++ Routier-backend/controller/favourtieController.js
@@ -1,106 +1,114 @@
-import Favorites, { findOne, findOneAndDelete, find } from '../model/favouritesModel.js';
-import { findById } from '../model/userModel.js';
-import { findById as _findById } from '../model/restaurantModel.js';
-import { findById as __findById } from '../model/todoModel.js';
-import { findById as ___findById } from '../model/placeinfoModel.js';
-import { findById as ____findById } from '../model/stayModel.js';
+import Favorites from '../model/favouritesModel.js';
+import User from '../model/userModel.js';
+import Restaurant from '../model/restaurantModel.js';
+import ThingToDo from '../model/todoModel.js';
+import PlaceInfo from '../model/placeinfoModel.js';
+import Stay from '../model/stayModel.js';
 
-
-export async function addFavorite(req, res) {
-    const { userId, itemName, itemType } = req.body;
-
-    try {
-        // Check if user exists
-        const user = await findById(userId);
-        if (!user) {
-            return res.status(404).json({ message: 'User not found' });
-        }
-
-        // Check if item exists
-        let item;
-        if (itemType === 'restaurant') {
-            item = await _findById(itemName);
-        } else if (itemType === 'todo') {
-            item = await __findById(itemName);
-        } else if (itemType === 'place') {
-            item = await ___findById(itemName);
-        } else if (itemType === 'stay') {
-            item = await ____findById(itemName);
-        }
-
-        if (!item) {
-            return res.status(404).json({ message: 'Item not found' });
-        }
-
-        // Check if item already exists in favorites
-        const favorite = await findOne({ userId, itemName, itemType });
-        if (favorite) {
-            return res.status(400).json({ message: 'Item already exists in favorites' });
-        }
-
-        // Create new favorite
-        const newFavorite = new Favorites({
-            userId,
-            itemName,
-            itemType,
-        });
-
-        await newFavorite.save();
-
-        res.json({ message: 'Favorite added successfully' });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Internal server error' });
-    }
-}
-export async function removeFavorite(req, res) {
-    const { userId, itemName, itemType } = req.body;
+class FavoritesController {
+  async addFavorite(req, res) {
+    const { itemId, itemType } = req.body;
 
     try {
-        const user = await findById(userId);
-        if (!user) {
-            return res.status(404).json({ message: 'User not found' });
-        }
+      // Check if user exists
+      const user = await User.findById(req.user._id);
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
 
-        // Check if item exists in favorites
-        const favorite = await findOneAndDelete({ userId, itemName, itemType });
-        if (!favorite) {
-            return res.status(404).json({ message: 'Item not found in favorites' });
-        }
+      // Check if item exists
+      let item;
+      switch (itemType) {
+        case 'restaurant':
+          item = await Restaurant.findById(itemId);
+          break;
+        case 'thingToDo':
+          item = await ThingToDo.findById(itemId);
+          break;
+        case 'placeInfo':
+          item = await PlaceInfo.findById(itemId);
+          break;
+        case 'stay':
+          item = await Stay.findById(itemId);
+          break;
+        default:
+          return res.status(400).json({ message: 'Invalid item type' });
+      }
 
-        res.json({ message: 'Favorite removed successfully' });
+      if (!item) {
+        return res.status(404).json({ message: 'Item not found' });
+      }
+
+      // Check if item already exists in favorites
+      const favorite = await Favorites.findOne({
+        userId: req.user._id,
+        itemId,
+        itemType,
+      });
+      if (favorite) {
+        return res.status(400).json({ message: 'Item already exists in favorites' });
+      }
+
+      // Create new favorite
+      const newFavorite = new Favorites({
+        userId: req.user._id,
+        itemId,
+        itemType,
+      });
+
+      await newFavorite.save();
+
+      res.json({ message: 'Favorite added successfully' });
     } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Internal server error' });
+      console.error(error);
+      res.status(500).json({ message: 'Internal server error' });
     }
-}
-export async function getFavorites(req, res) {
-    const userId = req.params.userId;
+  }
+
+  async removeFavorite(req, res) {
+    const { itemId, itemType } = req.body;
 
     try {
-        const user = await findById(userId);
-        if (!user) {
-            return res.status(404).json({ message: 'User not found' });
-        }
+      // Check if user exists
+      const user = await User.findById(req.user._id);
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
 
-        // Get favorites
-        const favorites = await find({ userId });
+      // Check if item exists in favorites
+      const favorite = await Favorites.findOneAndDelete({
+        userId: req.user._id,
+        itemId,
+        itemType,
+      });
+      if (!favorite) {
+        return res.status(404).json({ message: 'Item not found in favorites' });
+      }
 
-        for (const favorite of favorites) {
-            if (favorite.itemType === 'restaurant') {
-                favorite.item = await _findById(favorite.itemName);
-            } else if (favorite.itemType === 'todo') {
-                favorite.item = await __findById(favorite.itemName);
-            } else if (favorite.itemType === 'place') {
-                favorite.item = await ___findById(favorite.itemName);
-            } else if (favorite.itemType === 'stay') {
-                favorite.item = await ____findById(favorite.itemName);
-            }
-        }
-
-        res.json({ favorites });
+      res.json({ message: 'Favorite removed successfully' });
     } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Internal server error' });
+      console.error(error);
+      res.status(500).json({ message: 'Internal server error' });
     }
+  }
+
+  async getFavorites(req, res) {
+    try {
+      // Check if user exists
+      const user = await User.findById(req.user._id);
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+
+      // Get favorites
+      const favorites = await Favorites.find({ userId: req.user._id });
+
+      res.json({ favorites });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  }
 }
+
+export default new FavoritesController();
