576324eded41af6605e0e0490272c11102df4068
Anuj Contractor <63806427+anujcontractor@users.noreply.github.com>
2023-11-17 12:35:58 +0530

    update userRoutes

--- Routier-backend/routes/userRoutes.js
+++ Routier-backend/routes/userRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 const router = Router();
 import { authUser, registerUser, getUserProfile, logoutUser } from '../controller/userController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 router.post('/login', authUser);
 router.post('/register', registerUser);
-router.get('/profile', getUserProfile);
+router.get('/profile',protect, getUserProfile);
 router.post('/logout', logoutUser);
 
 export default router;
