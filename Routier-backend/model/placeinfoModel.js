529b3fe9fd0342a85f9efe3eaeaa7c4202cb54a1
Anuj Contractor <63806427+anujcontractor@users.noreply.github.com>
2023-11-18 11:48:47 +0530

    updated Models

--- Routier-backend/model/placeinfoModel.js
+++ Routier-backend/model/placeinfoModel.js
@@ -1,42 +1,55 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose;
 
-const placeSchema= new Schema({
-    name :{
-        type:String,
-        required:true
+const placeSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    description:{
-        type:String,
-        required:true
+    description: {
+      type: String,
+      required: true,
     },
-    images:{
-        type:Array,
-        required:true
-    },
+    images: [
+      {
+        type: Array,
+        required: true,
+      },
+    ],
     tags: {
-        type: [String], 
-        default: [],    
+      type: [String],
+      default: [],
     },
-    restaurants:[{
+    restaurants: [
+      {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'Restaurant',
-    }],
-    stays:[{
+        ref: "Restaurant",
+      },
+    ],
+    stays: [
+      {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'Stay',
-    }],
-    todos:[{
+        ref: "Stay",
+      },
+    ],
+    todos: [
+      {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'Todo',
-    }],
-    reviews:[{
+        ref: "Todo",
+      },
+    ],
+    reviews: [
+      {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'Review',
-    }],
-},{
-    timestamps:true
-});
+        ref: "Review",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Place = mongoose.model("Place", placeSchema);
 export default Place;
--- Routier-backend/model/restaurantModel.js
+++ Routier-backend/model/restaurantModel.js
@@ -1,78 +1,82 @@
-import mongoose from 'mongoose';
-const {Schema} = mongoose;
-const restaurantSchema=new Schema({
-    name :{
-        type:String,
-        required:true
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+const restaurantSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    address:{
-        type:String,
-        required:true
+    address: {
+      type: String,
+      required: true,
     },
     location: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Place",
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Place",
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    cusine: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    image: [
+      {
+        type: String,
         required: true,
       },
-    phone:{
-        type:String,
-        required:true
+    ],
+    description: {
+      type: String,
+      required: true,
     },
-    email :{
-        type:String,
-        required:true
+    veg: {
+      type: Boolean,
+      required: true,
     },
-    cusine:{
-        type:String,
-        required:true
+    nonveg: {
+      type: Boolean,
+      required: true,
     },
-    rating:{
-        type:Number,
-        required:true
+    type: {
+      //like fastfood,restaurant,cafe etc
+      type: String,
+      required: true,
     },
-    price:{
-        type:Number,
-        required:true
+    time: {
+      type: String,
+      required: true,
     },
-    image:{
-        type:String,
-        required:true
+    website: {
+      type: String,
     },
-    description:{
-        type:String,
-        required:true
-    },
-    veg:{
-        type:Boolean,
-        required:true
-    },
-    nonveg:{
-        type:Boolean,
-        required:true
-    },
-    type:{
-        //like fastfood,restaurant,cafe etc
-        type:String,
-        required:true
-    },
-    time:{
-        type:String,
-        required:true
-    },
-    website:{
-        type:String,
-    },
-    menu:{
-        type:String,
+    menu: {
+      type: String,
     },
     reviews: [
-        {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "Review",
-        },
-      ],
-    
-},{timestamps:true});
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Review",
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
-const Restaurant = mongoose.model('Restaurant',restaurantSchema);
+const Restaurant = mongoose.model("Restaurant", restaurantSchema);
 export default Restaurant;
