f9e54e12481e4eb631c695a7fe5a19c618bc239c
Anuj Contractor <63806427+anujcontractor@users.noreply.github.com>
2023-11-23 22:59:57 +0530

    review link

--- Routier-frontend/src/Components/PlaceSites/SiteInfo.jsx
+++ Routier-frontend/src/Components/PlaceSites/SiteInfo.jsx
@@ -111,9 +111,9 @@ function SiteInfo(props) {
             <div className='rbuttons'>
               <div className="button">
                 <img src={edit_icon} alt="icon" />
-                {/* <Link to={`/givereview/${siteid}?placeName=${encodeURIComponent(site?.name)}&placeImage=${encodeURIComponent(site?.image[0])}`}>
+<Link to={`/givereview/${siteid}?placeName=${encodeURIComponent(site?.name)}&placeImage=${encodeURIComponent(site?.image?.[0])}`}>
                 Review
-                </Link> */}
+                </Link>
                  <Link to={`/`}>
                 Review
                 </Link>
