6555bc90e29f5df915cd659518e2fa68d8f46f80
Anuj Contractor <63806427+anujcontractor@users.noreply.github.com>
2023-12-04 01:13:24 +0530

    created showUser route and controller

--- Routier-backend/controller/userController.js
+++ Routier-backend/controller/userController.js
@@ -163,4 +163,16 @@ const logoutUser = expressAsyncHandler(async (req, res) => {
   res.status(200).send({ message: "User logged out" });
 });
 
-export { authUser, registerUser, getUserProfile, logoutUser };
+const show = expressAsyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    res.status(200).json({
+      user,
+    });
+  } else {
+    res.status(404).json({ message: "User not found" });
+  }
+});
+
+export { authUser, registerUser, getUserProfile, logoutUser, show };
--- Routier-backend/routes/userRoutes.js
+++ Routier-backend/routes/userRoutes.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 const router = Router();
-import { authUser, registerUser, getUserProfile, logoutUser } from '../controller/userController.js';
+import { authUser, registerUser, getUserProfile, logoutUser,show } from '../controller/userController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 router.post('/login', authUser);
 router.post('/register', registerUser);
 router.get('/profile',protect, getUserProfile);
 router.post('/logout', logoutUser);
+router.get('/:id', show);
+
 
 export default router;
